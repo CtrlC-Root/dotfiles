e0b1d660a3bbd00a01fab3b00cdcda187673d8f3
Alexandru Barbur <root.ctrlc@gmail.com>
2014-11-13 21:44:49 -0500

    Add an initialization script for the fish shell.

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+# configure git settings
+if test ! -f ~/.gitconfig
+    git config --global user.name "Alexandru Barbur"
+    git config --global user.email "root.ctrlc@gmail.com"
+    git config --global push.default simple
+    git config --global color.ui auto
+end
+
+# golang
+set GOPATH $HOME/go
+if test -d "$GOPATH/bin"
+    set PATH $PATH $GOPATH/bin
+end
+
+# user binaries
+set PATH $PATH $HOME/bin

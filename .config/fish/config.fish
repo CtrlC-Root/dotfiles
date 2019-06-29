7ec5d53ff54da1953e8aeb1dfdc2be528bf8486e
Alexandru Barbur <root.ctrlc@gmail.com>
2019-06-28 22:09:29 -0400

    Reindent config.fish with spaces.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,135 +4,135 @@ set -l ctrlc_env_start (date '+%s')
 # check if we need to update the cache
 set -l ctrlc_platform (uname)
 if test "$ctrlc_platform" = "Linux"
-	set ctrlc_last_boot (date -d (last reboot | head -n 1 | sed -e 's/ \+/ /g' | cut -d ' ' -f5-8) '+%s')
+  set ctrlc_last_boot (date -d (last reboot | head -n 1 | sed -e 's/ \+/ /g' | cut -d ' ' -f5-8) '+%s')
 else if test "$ctrlc_platform" = "Darwin"
-	set ctrlc_last_boot (date -jf '%a %b %d %H:%M' '+%s' (last reboot | head -n 1 | sed -e 's/  */ /g' | cut -d ' ' -f3-6))
+  set ctrlc_last_boot (date -jf '%a %b %d %H:%M' '+%s' (last reboot | head -n 1 | sed -e 's/  */ /g' | cut -d ' ' -f3-6))
 else
-	# force an update every time
-	echo "$ctrlc_platform unknown platform"
-	set ctrlc_last_boot (date '%+s')
+  # force an update every time
+  echo "$ctrlc_platform unknown platform"
+  set ctrlc_last_boot (date '%+s')
 end
 
 if ! set -q ctrlc_env_cache
-	set -U ctrlc_env_cache 0
+  set -U ctrlc_env_cache 0
 end
 
 if test "$ctrlc_last_boot" -gt "$ctrlc_env_cache"
-	set ctrlc_env_update "$ctrlc_last_boot"
-	set -U ctrlc_env_cache "$ctrlc_last_boot"
+  set ctrlc_env_update "$ctrlc_last_boot"
+  set -U ctrlc_env_cache "$ctrlc_last_boot"
 end
 
 # detect preferred editor
 if set -q ctrlc_env_update
-	set -e EDITOR
+  set -e EDITOR
 
-	set -l editors vim nano vi
-	for editor in $editors
-		if which $editor > /dev/null ^&1
-			set -U -x EDITOR $editor
-			break
-		end
-	end
+  set -l editors vim nano vi
+  for editor in $editors
+    if which $editor > /dev/null ^&1
+      set -U -x EDITOR $editor
+      break
+    end
+  end
 end
 
 # configure git settings
 if test ! -f ~/.gitconfig
-	git config --global user.name "Alexandru Barbur"
-	git config --global user.email "root.ctrlc@gmail.com"
-	git config --global push.default simple
-	git config --global color.ui auto
+  git config --global user.name "Alexandru Barbur"
+  git config --global user.email "root.ctrlc@gmail.com"
+  git config --global push.default simple
+  git config --global color.ui auto
 end
 
 # detect python tools
 if set -q ctrlc_env_update
-	set -e ctrlc_python
-	set -e ctrlc_pip
+  set -e ctrlc_python
+  set -e ctrlc_pip
 
-	set -l python_tools 'python3 pip3' 'python2 pip2' 'python pip'
-	for tools in $python_tools
-		# locate toolset binaries
-		set -l binaries
-		for binary in (echo $tools | tr ' ' '\n')
-			if which $binary > /dev/null ^&1
-				set binaries $binaries $binary
-			end
-		end
+  set -l python_tools 'python3 pip3' 'python2 pip2' 'python pip'
+  for tools in $python_tools
+    # locate toolset binaries
+    set -l binaries
+    for binary in (echo $tools | tr ' ' '\n')
+      if which $binary > /dev/null ^&1
+        set binaries $binaries $binary
+      end
+    end
 
-		# use this toolset if all binaries are available
-		if test (count $binaries) -ge 2
-			set -U ctrlc_python $binaries[1]
-			set -U ctrlc_pip $binaries[2]
-			break
-		end
-	end
+    # use this toolset if all binaries are available
+    if test (count $binaries) -ge 2
+      set -U ctrlc_python $binaries[1]
+      set -U ctrlc_pip $binaries[2]
+      break
+    end
+  end
 end
 
 # user binaries
 if test -d "$HOME/bin"
-	set -x PATH $PATH $HOME/bin
+  set -x PATH $PATH $HOME/bin
 end
 
 # local binaries
 if test -d "$HOME/.local/bin"
-	set -x PATH $PATH $HOME/.local/bin
+  set -x PATH $PATH $HOME/.local/bin
 end
 
 # gpg
 if which gpg > /dev/null ^&1
-	# https://stackoverflow.com/a/41054093
-	set -x GPG_TTY (tty)
+  # https://stackoverflow.com/a/41054093
+  set -x GPG_TTY (tty)
 end
 
 # virtualfish
 if set -q ctrlc_env_update
-	set -e ctrlc_enable_virtualfish
-	set -e ctrlc_virtualfish_config
+  set -e ctrlc_enable_virtualfish
+  set -e ctrlc_virtualfish_config
 
-	if set -q ctrlc_python ctrlc_pip
-	    if eval $ctrlc_pip show virtualfish > /dev/null
-		set -U ctrlc_enable_virtualfish "$ctrlc_last_boot"
-		set -U ctrlc_virtualfish_config (eval $ctrlc_python -m virtualfish)
-	    end
-	end
+  if set -q ctrlc_python ctrlc_pip
+    if eval $ctrlc_pip show virtualfish > /dev/null
+      set -U ctrlc_enable_virtualfish "$ctrlc_last_boot"
+      set -U ctrlc_virtualfish_config (eval $ctrlc_python -m virtualfish)
+    end
+  end
 end
 
 if set -q ctrlc_enable_virtualfish
-	eval $ctrlc_virtualfish_config
+  eval $ctrlc_virtualfish_config
 end
 
 # brew
 if which brew > /dev/null ^&1
-    set -x ARCHFLAGS "-arch x86_64"
-    set -x PATH (brew --prefix)/bin (brew --prefix)/sbin $PATH
-    set -x HOMEBREW_NO_ANALYTICS 1
+  set -x ARCHFLAGS "-arch x86_64"
+  set -x PATH (brew --prefix)/bin (brew --prefix)/sbin $PATH
+  set -x HOMEBREW_NO_ANALYTICS 1
 end
 
 # golang
 set -x GOPATH $HOME/go
 if test -d "$GOPATH/bin"
-    set -x PATH $PATH $GOPATH/bin
+  set -x PATH $PATH $GOPATH/bin
 end
 
 # powerline prompt
 if set -q ctrlc_env_update
-	set -e ctrlc_enable_powerline
-	set -e ctrlc_powerline_root 
+  set -e ctrlc_enable_powerline
+  set -e ctrlc_powerline_root
 
-	if set -q ctrlc_python ctrlc_pip
-	    if eval $ctrlc_pip show powerline-status > /dev/null
-		set -U ctrlc_enable_powerline "$ctrlc_last_boot"
-		set -U ctrlc_powerline_root (eval $ctrlc_pip show powerline-status | awk '$1 == "Location:" { print $2; }')
-	    end
-	end
+  if set -q ctrlc_python ctrlc_pip
+    if eval $ctrlc_pip show powerline-status > /dev/null
+      set -U ctrlc_enable_powerline "$ctrlc_last_boot"
+      set -U ctrlc_powerline_root (eval $ctrlc_pip show powerline-status | awk '$1 == "Location:" { print $2; }')
+    end
+  end
 end
 
 if set -q ctrlc_enable_powerline
-	# start the daemon in the background
-	powerline-daemon -q
+  # start the daemon in the background
+  powerline-daemon -q
 
-	# configure the prompt
-	set fish_function_path $fish_function_path "$ctrlc_powerline_root/powerline/bindings/fish"
-	powerline-setup
+  # configure the prompt
+  set fish_function_path $fish_function_path "$ctrlc_powerline_root/powerline/bindings/fish"
+  powerline-setup
 end
 
 # record end time and display total load time
